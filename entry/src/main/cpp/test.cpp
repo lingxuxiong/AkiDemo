91bb12a2fd060085d9756f9bc3c560b61e026ac4
lingxx <lingxx@lenovoimage.com>
2024-05-08 10:35:24 +0800

    test out SayHello flow in Aki

---
+++ entry/src/main/cpp/test.cpp
@@ -0,0 +1,16 @@
+//
+// Created on 2024/5/8.
+//
+// Node APIs are not fully supported. To solve the compilation error of the interface cannot be found,
+// please include "napi/native_api.h".
+
+#include <aki/jsbind.h>
+#include <string>
+
+std::string SayHello(std::string msg) { return msg + " too."; }
+
+// Step 1 注册 AKI 插件
+JSBIND_ADDON(entry) // 注册 AKI 插件名: 即为编译*.so名称，规则与NAPI一致
+
+// Step 2 注册 FFI 特性
+JSBIND_GLOBAL() { JSBIND_FUNCTION(SayHello); }
