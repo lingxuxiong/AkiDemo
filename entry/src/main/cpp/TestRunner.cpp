81ad2dc8c375cc346c82f2fe197617790ddc852c
lingxx <lingxx@lenovoimage.com>
2024-05-08 15:06:37 +0800

    bind class and class methods in c++

---
+++ entry/src/main/cpp/TestRunner.cpp
@@ -0,0 +1,29 @@
+//
+// Created on 2024/5/8.
+//
+// Node APIs are not fully supported. To solve the compilation error of the interface cannot be found,
+// please include "napi/native_api.h".
+
+#include <string>
+#include <aki/jsbind.h>
+
+class TaskRunner {
+public:
+    TaskRunner() = default;
+    std::string DoTask() {
+        AKI_LOG(INFO) << "DoTask";
+        return "done.";
+    }
+};
+
+JSBIND_CLASS(TaskRunner) {
+    JSBIND_CONSTRUCTOR<>();
+    JSBIND_PMETHOD(DoTask);
+}
+
+int AsyncTaskReturnInt2() {
+    AKI_LOG(INFO) << "[Aki] run async task2";
+    return rand(); 
+}
+
+JSBIND_GLOBAL() { JSBIND_PFUNCTION(AsyncTaskReturnInt2, "asyncTaskReturnInt2"); }
--- entry/src/main/cpp/types/libentry/index.d.ts
+++ entry/src/main/cpp/types/libentry/index.d.ts
@@ -2,3 +2,9 @@ export const add: (a: number, b: number) => number;
 export const SayHello: (msg: string) => string;
 export const callArkTSStaticMethod: (obj: object) => string;
 export const asyncTaskReturnInt: () => Promise<number>;
+export const asyncTaskReturnInt2: () => Promise<number>;
+
+export class TaskRunner {
+  TaskRunner();
+  DoTask: () => Promise<number>
+}
